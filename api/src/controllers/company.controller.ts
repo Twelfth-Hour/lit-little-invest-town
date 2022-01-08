eed80e97fd0b727b4c8b2999a6371f6a38e0637d
Sahil Bondre <sahilbondre@gmail.com>
2022-01-08 17:22:40 +0530

    add sort + filter to companies api

--- api/src/controllers/company.controller.ts
+++ api/src/controllers/company.controller.ts
@@ -1,10 +1,37 @@
 import { RequestHandler, Request, Response } from "express";
-import { getAllCompaniesService } from "../services/company.service";
+import Company from "../models/company.model";
+import {
+  getAllCompaniesService,
+  getCompaniesByRisk,
+} from "../services/company.service";
 
 export const getAllCompanies: RequestHandler = async (
-  _req: Request,
+  req: Request,
   res: Response
 ) => {
-  const companies = await getAllCompaniesService();
+  const risk = req.query.risk as string;
+  const sortBy = req.query.sortBy as string;
+  const reverse = req.query.reverse as string;
+
+  let companies: Company[] = [];
+
+  if (["low", "moderate", "high"].includes(risk)) {
+    companies = await getCompaniesByRisk(risk);
+  } else {
+    companies = await getAllCompaniesService();
+  }
+
+  if (sortBy === "marketCap") {
+    companies = companies.sort((a, b) => a.market_cap - b.market_cap);
+  }
+
+  if (sortBy === "name") {
+    companies = companies.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  if (reverse === "true") {
+    companies = companies.reverse();
+  }
+
   res.status(200).send(companies.map((company) => company.toDAO()));
 };
