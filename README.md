5d2014ad8ebc74ed3dcfbdc65d3f87b810937ff9
AemieJ <aemie.j@hotmail.co.uk>
2022-01-08 18:36:44 +0530

    create single stalk page static

---
+++ README.md
@@ -0,0 +1,58 @@
+# LIT: Little Invest Town
+
+`Finance Management`
+
+# Problem
+
+Stock markets are hard to analyze and there has been a surge in the number of people starting to invest in the markets. Stock markets are liquid, available on mobile, good returns.
+
+Stock Market and Company Profile Numbers don’t make sense
+
+# Solution
+
+<aside>
+💡 Starbucks Coffee at Rs 20, Brand New iPhone at Rs 10K ⇒ Good Buy
+ITC ⇒ Rs 220/Share ⇒ ????
+ICICI ⇒ Rs 1.6 Lakh Crore Revenue ⇒ ????
+
+</aside>
+
+Scale down company profiles and map them to individual financial profiles.
+
+### Example Profile
+
+1. Alice makes and sells oil and electricity
+2. In 2021 Alice made 50 Lakh Rupees
+3. Last year she made 61 Lakh Rupees
+4. Out of this, she could save up 5 Lakh Rupees which is 20% more than what she saved last year
+5. Alice bought a new oil making machine for 3 Lakh Rupees
+6. Alice also invested 5 Lakh rupees in Mutual Funds
+7. Alice also paid her loan by 2 Lakh rupees and her loan now is 3.5 Lakhs
+
+# Roadmap
+
+- [x]  Skeletons and ER Diagrams
+- [x]  Landing Page ⇒ Intuitive to the average joe
+- [ ]  All Stocks
+- [ ]  Stock Profile
+- [ ]  Get Data for Companies
+- [ ]  Scale Data and Derive Insights from it
+
+# Getting Data
+
+1. TickerTape Undocumented API
+2. BSE/NSE Endpoints for Stock Prices
+3. Company Reports
+4. Web Scrape
+
+1. ☎ Communication Services
+2. 🚗 Consumer Discretionary
+3. ☕ Consumer Staples
+4. ⚡ Energy
+5. 💸 Financials
+6. 🚑 Healthcare
+7. 🏭 Industrials
+8. 💻 Information Technology
+9. 💎 Materials
+10. 🏠 Real Estate
+11. 💧 Utilities
--- client/pages/stock/[key].js
+++ client/pages/stock/[key].js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-// import { server } from "../../config/server.js";
+import { server } from "../../config/server.js";
 import { Row, Col, InputGroup } from "react-bootstrap";
 import styles from "../../styles/Profile.module.css";
 import { useState } from "react";
 
-const Detail = ({ stock }) => {
+const Detail = ({ stock, similar }) => {
   /* eslint-disable no-unused-vars */
   const [currPrice, setCurr] = useState(stock.currPrice);
 
@@ -20,14 +20,14 @@ const Detail = ({ stock }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Row xs={12} lg={12} md={12} className={styles.wallpaper}>
-        <Col>
+        {/* <Col xs={4} md={4} lg={5}>
           <img
             src="/profile_bg.svg"
             alt="background profile"
             className={styles.bg_img}
           />
-        </Col>
-        <Col xs={12} md={9} lg={7}>
+        </Col> */}
+        <Col>
           <h3 className={styles.title}>
             Nobody beats {stock.profile} in explaining insights of{" "}
             {stock.symbol}
@@ -55,14 +55,64 @@ const Detail = ({ stock }) => {
             </Col>
           </Row>
         </Col>
+
+        <Col xs={4} md={4} lg={4} className={styles.similar_container}>
+          <h3>Similar Profiles</h3>
+          {similar.map((item) => {
+            return (
+              <Row key={item.symbol} style={{ marginTop: ".5rem" }}>
+                <Col xs={3} md={3} lg={3}>
+                  <img src={item.avatar} className={styles.similar_img} />
+                </Col>
+                <Col>
+                  <Row>
+                    <a
+                      onClick={(e) => {
+                        e.preventDefault();
+                        window.open(`${server}/stock/${item.symbol}`, "_blank");
+                      }}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <div>
+                        <span className={styles.similar_title}>
+                          {item.profile}
+                        </span>
+                        {" | "}
+                        <span className={styles.similar_name}>
+                          {item.symbol}
+                        </span>
+                      </div>
+                    </a>
+                  </Row>
+                  <Row>
+                    <span className={styles.similar_name}>{item.name}</span>
+                  </Row>
+                </Col>
+              </Row>
+            );
+          })}
+        </Col>
       </Row>
 
       <Row>
-        <Col xs={3} md={3} lg={3}>
-          <div className={styles.insight_title}>Insights</div>
+        <Col xs={4} md={4} lg={4}>
+          <div className={styles.insight_title}>
+            <span>Insights</span>
+            <div className={styles.triangle_left}></div>
+          </div>
         </Col>
-        <Col>
-          <div className={styles.insight_container}></div>
+        <Col className={styles.insight_container}>
+          <div>
+            {stock.insight.map((item, idx) => {
+              return (
+                <div key={idx}>
+                  <span key={idx}>
+                    {idx + 1}. {item}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
         </Col>
       </Row>
     </>
@@ -83,6 +133,30 @@ export const getServerSideProps = (context) => {
     "Alice also paid her loan by 2 Lakh rupees and her loan now is 3.5 Lakhs",
   ];
 
+  let similarProfiles = [
+    {
+      name: "Reliance Industries Limited",
+      symbol: "RELIANCE",
+      profile: "Raveen",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C4D03AQGuSVxEgwSDaA/profile-displayphoto-shrink_800_800/0/1616911585841?e=1646870400&v=beta&t=cxUKCI7jqSaOEak9BegiCn7MivEturemGIGAPKv2JTI",
+    },
+    {
+      name: "Kotak Mahindra Bank Limited",
+      symbol: "KOTAKBANK",
+      profile: "Kshitij",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQG-PDOJqcsBtA/profile-displayphoto-shrink_800_800/0/1627758064351?e=1646870400&v=beta&t=tbC0f2FGiiXfa0SImWFIp-eWRl_Ur2QxARj1LVsp6w4",
+    },
+    {
+      name: "Sun Pharmaceutical Industries Limited",
+      symbol: "SUNPHARMA",
+      profile: "Sunny",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5103AQHTbCiP412O8w/profile-displayphoto-shrink_800_800/0/1580654448686?e=1646870400&v=beta&t=lppNzlQdBstfRP_4yHb4Qsa6HkQSdxZrTMPdbqRlkuA",
+    },
+  ];
+
   let stock = {
     name: "Tata Consultancy Services Limited",
     symbol: key,
@@ -98,6 +172,7 @@ export const getServerSideProps = (context) => {
   return {
     props: {
       stock,
+      similar: similarProfiles,
     },
   };
 };
