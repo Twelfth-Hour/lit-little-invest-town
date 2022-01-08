f6d3eef40919a0d10f130fe9f12e1b376fa19d22
AemieJ <aemie.j@hotmail.co.uk>
2022-01-09 03:21:45 +0530

    change client to production

--- client/config/server.js
+++ client/config/server.js
@@ -1,4 +1,4 @@
-const dev = "development";
+const dev = "production";
 
 export const server =
-  dev == "development" ? "http://localhost:3000" : "https://github.com/";
+  dev == "development" ? "http://localhost:3000" : "https://little-invest-town.vercel.app";
